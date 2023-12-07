b5e03f11e0d7fd6a3ab90fd4c23b12d0539718c1
Divya R <kwrdivyaraj100@gmail.com>
2023-12-07 14:48:43 +0530

    Updated Header component to conditionally render tabs and buttons based on login status

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { AppBar, Box, Button, Tab, Tabs, Toolbar, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
 
-function Header() {
+function Header(props) {
     const [value, setValue] = useState();
     return (
         <AppBar position='sticky' sx={{ background: 'linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(48,9,121,1) 35%, rgba(0,212,255,1) 100%)' }}>
@@ -10,17 +10,16 @@ function Header() {
                 <Typography variant='h4'>
                     Blog App
                 </Typography>
-                <Box display={"flex"} marginLeft={'auto'} marginRight={"auto"}>
+                {props.isLoogedIn && <Box display={"flex"} marginLeft={'auto'} marginRight={"auto"}>
                     <Tabs textColor='inherite' value={value} onChange={(e, val) => setValue(val)}>
                         <Tab LinkComponent={Link} to="/blogs" label="All Blogs" />
                         <Tab LinkComponent={Link} to="/myBlogs" label="My Blogs" />
                     </Tabs>
-                </Box>
+                </Box>}
                 <Box display={"flex"} marginLeft={"auto"}>
                     <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Login</Button>
                     <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Signup</Button>
-                    <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>
-                </Box>
+                    {props.isLoogedIn && <Button LinkComponent={Link} to="/auth" variant='contained' sx={{ margin: 1, borderRadius: 10 }} color='warning'>Logout</Button>}                </Box>
             </Toolbar>
         </AppBar>
     )
