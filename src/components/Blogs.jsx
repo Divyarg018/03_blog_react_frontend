81e26539d52cb0023ab840466bea10b26bfc5f74
Divya R <kwrdivyaraj100@gmail.com>
2023-12-08 15:28:49 +0530

    Fetching and displaying blog data using axios and useEffect

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,6 +1,20 @@
 import React from 'react';
 
 function Blogs() {
+    const [blogs, setBlogs] = useState();
+
+    async function sendRequest() {
+        const res = await axios
+            .get("http://localhost:5000/api/blog")
+            .catch(err => console.log(err))
+        const data = await res.data;
+        return data;
+    }
+    useEffect(() => {
+        sendRequest()
+            .then(data => setBlogs(data.blogs));
+    }, []);
+    console.log(blogs);
     return (
         <div>Blogs</div>
     )
