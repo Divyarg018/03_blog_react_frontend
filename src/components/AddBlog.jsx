ded995937bd825170734ad38182baa9d5ddaa434
Divya R <kwrdivyaraj100@gmail.com>
2023-12-09 12:24:00 +0530

    Adding sendRequest function to post blog data to API using axios

--- src/components/AddBlog.jsx
+++ src/components/AddBlog.jsx
@@ -1,17 +1,29 @@
+import axios from 'axios';
 import React, { useState } from 'react';
 
+const labelStyle = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }
 function AddBlog() {
     const [inputs, setInputs] = useState({
         title: "", description: "", imageURL: ""
-      })
-    
-      function handleChange(e) {
+    })
+
+    function handleChange(e) {
         setInputs((prevState) => ({
-          ...prevState,
-          [e.target.name]: e.target.value
+            ...prevState,
+            [e.target.name]: e.target.value
         })
         );
-      }
+    }
+    async function sendRequest() {
+        const res = await axios.post(`http://localhost:5000/api/blog/add`, {
+            title: inputs.title,
+            description: inputs.description,
+            image: inputs.imageURL,
+            user: localStorage.getItem("userID")
+        }).catch(err => console.log(err))
+        const data = await res.data;
+        return data;
+    }
     return (
         <div>AddBlog</div>
     )
