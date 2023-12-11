8d48a8f616eb353d7b6a95852e542aae9976ea8b
Divya R <kwrdivyaraj100@gmail.com>
2023-12-11 11:01:32 +0530

    Adding fetch details and useEffect functions

--- src/components/BlogDetail.jsx
+++ src/components/BlogDetail.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 const labelStyle = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }
 function BlogDetail(props) {
@@ -16,6 +17,22 @@ function BlogDetail(props) {
         })
         );
     }
+
+    async function fetchDetails() {
+        const res = await axios
+            .get(`http://localhost:5000/api/blog/${id}`)
+            .catch(err => console.log(err))
+        const data = await res.data;
+        return data;
+    }
+    useEffect(() => {
+        fetchDetails().then(data => {
+            setBlog(data.blog)
+            setInputs({ title: data.blog.title, description: data.blog.description })
+
+        })
+
+    }, [id]);
     return (
         <div>BlogDetail</div>
     )
