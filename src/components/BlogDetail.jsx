fedfd6149b862d2073c1fbba3e5a5ee5a521a12b
Divya R <kwrdivyaraj100@gmail.com>
2023-12-11 11:06:22 +0530

    Adding form component to update blog post

--- src/components/BlogDetail.jsx
+++ src/components/BlogDetail.jsx
@@ -1,6 +1,8 @@
+import { Box, Button, InputLabel, TextField, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+
 const labelStyle = { mb: 1, mt: 2, fontSize: '24px', fontWeight: 'bold' }
 function BlogDetail(props) {
     const navigate = useNavigate();
@@ -33,7 +35,7 @@ function BlogDetail(props) {
         })
 
     }, [id]);
-    
+
     async function sendRequest() {
         const res = await axios.put(`http://localhost:5000/api/blog/update/${id}`, {
             title: inputs.title,
@@ -53,7 +55,35 @@ function BlogDetail(props) {
             .then(() => navigate("/myBlogs"))
     }
     return (
-        <div>BlogDetail</div>
+        <div>
+            {inputs &&
+                <form onSubmit={handleSubmit}>
+                    <Box
+                        border={3}
+                        borderColor='linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,19,121,1) 0%, rgba(0,212,255,1) 100%)'
+                        borderRadius={10}
+                        boxShadow="10px 10px 20px #ccc"
+                        padding={3}
+                        margin={"auto"}
+                        marginTop={3}
+                        display='flex'
+                        flexDirection={'column'}
+                        width={"80%"}
+                    >
+                        <Typography fontWeight={'bold'}
+                            padding={3}
+                            color="grey"
+                            variant='h2'
+                            textAlign='center'>Update Blog</Typography>
+                        <InputLabel sx={labelStyle}>Title</InputLabel>
+                        <TextField name='title' onChange={handleChange} value={inputs.title} margin='auto' variant='outlined' />
+                        <InputLabel sx={labelStyle}>Description</InputLabel>
+                        <TextField name='description' onChange={handleChange} value={inputs.description} margin='auto' variant='outlined' />
+                        <Button type="submit" sx={{ mt: 2, borderRadius: 4 }} variant='contained' color='warning'>Submit</Button>
+                    </Box>
+                </form>
+            }
+        </div>
     )
 }
 
